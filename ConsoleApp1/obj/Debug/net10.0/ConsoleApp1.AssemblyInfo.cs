8c26bede707d6e316d28d5a37e0b59adda00b961
Samuel Trilles <samueltrilles9@gmail.com>
2026-01-19 05:45:51 +0000

    Task3

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250eb8f5769bb631e28616269e22c927c7c5fd9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5762b3a9323b17af35cc66a1a46bbf1b2abb533b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
